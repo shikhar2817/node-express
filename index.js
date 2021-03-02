4a50d1879646d7dad38e3d5856b42da28cf5cf31
Shikhar Singh <shikharsingh0076@gmail.com>
2021-03-02 17:36:48 +0530

    Express Simple REST

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,6 +9,49 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes' , (req, res, next) => {
+    res.sendStatus = 200;
+    res.setHeader('Content-Type' , 'text/plain');
+    next(); 
+});
+
+app.get('/dishes' , (req, res, next) => {
+    res.end('Will Send you all the dishes to you!');
+});
+
+app.post('/dishes' , (req, res, next) => {
+    res.end('Will all the dishe: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes' , (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operation is not supported on /dishes');
+});
+
+app.delete('/dishes' , (req, res, next) => {
+    res.end('Deleting all the dishes!');
+});
+
+app.get('/dishes/:dishId' , (req, res, next) => {
+    res.end('Will Send details of the dishes : ' + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId' , (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation is not supported on /dishes/' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId' , (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details ' + req.body.description );
+});
+
+app.delete('/dishes/:dishId' , (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
 app.use(express.static(__dirname+ '/public'));
 
 app.use((req, res, next) => {
